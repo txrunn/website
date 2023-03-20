7f00a2f82f26f71eb10bb63dad3d80d3b59621cc
Tarun Gunaseelan <tarun.gunaseelan@gmail.com>
2023-03-20 16:57:44 -0400

    Added project liveview

--- lib/website_web/live/project_live.ex
+++ lib/website_web/live/project_live.ex
@@ -33,9 +33,11 @@ defmodule WebsiteWeb.ProjectLive do
   def fetch_projects(socket) do
     url = "https://api.github.com/users/txrunn/repos"
     headers = [{"User-Agent", "website"}]
+
     case HTTPoison.get(url, headers) do
       {:ok, %{status_code: 200, body: body}} ->
         projects = Jason.decode!(body)
+        socket = assign(socket, :projects, projects)
         {:ok, assign(socket, :projects, projects)}
       _ ->
         {:ok, socket}
