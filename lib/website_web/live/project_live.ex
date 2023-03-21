5b179cbf0c7c8d71d77eaba96f44e706f76a9705
Tarun Gunaseelan <tarung@terpmail.umd.edu>
2023-03-20 21:45:09 -0400

    Rewind time

--- lib/website_web/live/project_live.ex
+++ lib/website_web/live/project_live.ex
@@ -24,7 +24,21 @@ defmodule WebsiteWeb.ProjectLive do
       context: repo
     }, [{"User-Agent", "website"}, {"Accept", "application/vnd.github.v3.text+html"}]).body) |> String.strip
     {:noreply, assign(socket, :selected_repo, %{name: repo, html: html})}
+    end
+    
+    defp fetch_repo_readme(repo_full_name) do
+    url = "https://api.github.com/repos/#{repo_full_name}/readme"
+    headers = [{"User-Agent", "website"}]
+    case HTTPoison.get(url, headers) do
+      {:ok, %{status_code: 200, body: body}} ->
+        content = Jason.decode!(body)
+        Base.decode64!(content["content"]) |> String.strip
+      _ ->
+        ""
+    end
   end
+
+
 
   def handle_event("close_readme", _, socket) do
     {:noreply, assign(socket, :selected_repo, nil)}
@@ -42,47 +56,28 @@ defmodule WebsiteWeb.ProjectLive do
     end
   end
   
-  defp fetch_workflow_status(repo_full_name) do
-    url = "https://api.github.com/repos/#{repo_full_name}/actions/runs"
-    headers = [{"User-Agent", "website"}]
-    case HTTPoison.get(url, headers) do
-      {:ok, %{status_code: 200, body: body}} ->
-        workflow_runs = Jason.decode!(body)
-        if hd(workflow_runs["workflow_runs"]) do
-          latest_workflow_run_id = hd(workflow_runs["workflow_runs"])["id"]
-          url = "https://api.github.com/repos/#{repo_full_name}/actions/runs/#{latest_workflow_run_id}/jobs"
-        case HTTPoison.get(url, headers) do
-          {:ok, %{status_code: 200, body: body}} ->
-            jobs = Jason.decode!(body)
-            %{status: hd(jobs["jobs"])["status"], conclusion: hd(jobs["jobs"])["conclusion"]}
-        end
-        else
-          nil
-        end
-    end
-  end
- 
-  defp fetch_repo_readme(repo_full_name) do
-    url = "https://api.github.com/repos/#{repo_full_name}/readme"
-    headers = [{"User-Agent", "website"}]
-    case HTTPoison.get(url, headers) do
-      {:ok, %{status_code: 200, body: body}} ->
-        content = Jason.decode!(body)
-        Base.decode64!(content["content"]) |> String.strip
-      _ ->
-        ""
-    end
-  end
+  
   
   defp render_card(project) do
     repo_full_name = project["full_name"]
-    workflow_status = fetch_workflow_status(repo_full_name)
-
+    
     ~s"""
     <div class="project-card" phx-click="view_readme" phx-value-repo="#{repo_full_name}">
       <h3><%= project["name"] %></h3>
       <p><%= project["description"] || "" %></p
     </div>
     """
-   end
+  end
+
+    def render(assigns) do
+    selected_repo = assigns[:selected_repo]
+
+    projects = assigns[:projects] || []
+    card_elements =
+      for project <- projects, do: render_card(project)
+
+    ~H"""
+    
+    """
+  end
 end
